1efd3b9131a29b7185962c8fd3187fc2824928b4
lbdm44 <lewis.miller.github@pm.me>
2022-03-11 17:10:50 -0800

    test: adding tests for discover packages

--- src/discovery/__tests__/discover-packages.test.ts
+++ src/discovery/__tests__/discover-packages.test.ts
@@ -1,8 +1,44 @@
+import * as path from 'path';
 import { describe, expect, test } from 'vitest';
 import { discoverPackages } from '../discover-packages';
 
 describe('discover-packages', () => {
-  test('it should exist', () => {
-    expect(discoverPackages).toBeDefined();
+  test('it should discover packages', () => {
+    const root = path.resolve(__dirname, './__fixtures__/simple-app');
+    const packagePaths = discoverPackages({
+      ignorePaths: [],
+      root,
+    });
+
+    expect(packagePaths).toEqual([root]);
+  });
+
+  test('it should discover packages in a complex app', () => {
+    const root = path.resolve(
+      __dirname,
+      './__fixtures__/complex-app/packages/'
+    );
+
+    const packagePaths = discoverPackages({
+      ignorePaths: [],
+      root,
+    });
+
+    // The fixure has three packages, `pkg-c` does not have a `package.json` file and should not be discovered.
+    expect(packagePaths).toEqual([`${root}/pkg-a`, `${root}/pkg-b`]);
+  });
+
+  test('it should honor ignored packages', () => {
+    const root = path.resolve(
+      __dirname,
+      './__fixtures__/complex-app/packages/'
+    );
+
+    const packagePaths = discoverPackages({
+      ignorePaths: ['*pkg-a'],
+      root,
+    });
+
+    expect(packagePaths).toEqual([`${root}/pkg-b`]);
   });
 });
